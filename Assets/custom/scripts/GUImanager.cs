1da87a3a93d4a4660d9beec9e3a77d0d9fd028ee
DANCORP\Dan <danielbatista.contato@gmail.com>
2016-11-13 20:13:18 -0800

    finished scene storyPlayer

--- Assets/custom/scripts/GUImanager.cs
+++ Assets/custom/scripts/GUImanager.cs
@@ -4,12 +4,18 @@ using UnityEngine.UI;
 
 public class GUImanager : MonoBehaviour {
 	RawImage backgroundImage;
+	RawImage storyImage;
 	Text chapterText;
 
-	public string[] chapters;
-	public int chapterIndex=0;
-
 	public float fadeTime = 0.5f;
+	public int frameTextIndex=0;
+
+	public string[] frameTexts;
+	public Texture[] storyImages;
+	public int[] storyImageChapters;
+
+	private int storyImageIndex=0;
+
 	private float fadeStart;
 	private bool fadein;
 	private bool fadeout;
@@ -17,25 +23,35 @@ public class GUImanager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		chapterText = GetComponentInChildren<Text> ();
-		backgroundImage = GetComponentInChildren<RawImage> ();
+		backgroundImage = GameObject.Find(gameObject.name+"/BlackLayer4fade").GetComponent<RawImage> ();
+		storyImage = GameObject.Find(gameObject.name+"/storyImage").GetComponent<RawImage> ();
 		backgroundImage.CrossFadeAlpha (1f,0f,true);
-		chapterText.text = chapters [chapterIndex];
-		Debug.Log ("game started...");
+		chapterText.text = frameTexts [frameTextIndex];
+
+		if (storyImages.Length == storyImageChapters.Length) {
+			if (frameTextIndex == 0) {
+				Debug.Log ("game started from scratch...");
+			}
+			else {
+				Debug.Log ("game started from some specific point...");
+			}
+			InitStory ();
+		}
+		else{
+			Debug.Log ("The arrays storyImages and storyImageChapters shall have the same length, because one is the specification for what chapter each image is.");
+			Application.Quit ();
+			Time.timeScale = 0;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown ("space")) {
-			if (chapterIndex == 0) {
-				backgroundImage.CrossFadeAlpha (1f,0f,true);
-				changeText ();
-				fadein = true;
-				backgroundImage.CrossFadeAlpha (0f,fadeTime,false);
-			} else {
+			if (frameTextIndex != 0) {
 				backgroundImage.CrossFadeAlpha (1f,fadeTime,false);
 				fadeout = true;
+				fadeStart = Time.time;
 			}
-			fadeStart = Time.time;
 		}
 		if (fadein) {
 			if (Time.time - fadeStart >= fadeTime) {
@@ -44,7 +60,7 @@ public class GUImanager : MonoBehaviour {
 		}
 		if (fadeout) {
 			if (Time.time - fadeStart >= fadeTime) {
-				changeText ();
+				nextFrame ();
 				fadeout = false;
 				fadein = true;
 				backgroundImage.CrossFadeAlpha (0f,fadeTime,false);
@@ -52,14 +68,49 @@ public class GUImanager : MonoBehaviour {
 			}
 		}
 	}
-
-	void changeText(){
-		chapterIndex += 1;
-		if (chapterIndex >= chapters.Length) {
-			Debug.Log ("ok, reached the end, for now let us go back to the first chapter");
-			chapterIndex = 0;
+	void InitStory(){
+		//prepare story image
+		int targetFrameText = frameTextIndex+1;
+		for (int i = 0; i < storyImageChapters.Length; i++) {
+			if (storyImageChapters [i] >= targetFrameText) {
+				storyImageIndex = i;
+				break;
+			}
 		}
-
-		chapterText.text = chapters [chapterIndex];
+		//hide storyImage
+		storyImage.enabled = false;
+		backgroundImage.CrossFadeAlpha (1f,0f,true);
+		nextFrame ();
+		fadein = true;
+		backgroundImage.CrossFadeAlpha (0f,2*fadeTime,false);
+		fadeStart = Time.time;
+	}
+	void nextFrame(){
+		//check if there is a story image
+		if (storyImageIndex >= 0) {
+			if (storyImageChapters [storyImageIndex] == frameTextIndex)
+				nextImage ();
+			else
+				nextText ();
+		} 
+		else{
+			nextText ();				
+		}
+	}
+	void nextText(){
+		storyImage.enabled = false;
+		frameTextIndex += 1;
+		if (frameTextIndex >= frameTexts.Length) {
+			Debug.Log ("ok, reached the end, for now let us go back to the first chapter");
+			frameTextIndex = 0;
+		}
+		chapterText.text = frameTexts [frameTextIndex];		
+	}
+	void nextImage(){
+		storyImage.enabled = true;
+		storyImage.texture = storyImages[storyImageIndex];
+		storyImageIndex += 1;
+		if (storyImageIndex >= storyImages.Length)
+			storyImageIndex = -1;
 	}
 }
---
+++ Assets/custom/scripts/TestGUI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TestGUI : MonoBehaviour {
+	public RawImage handButtonImg;
+	public Selectable handButton;
+	public EventSystem eventListener;
+
+	// Use this for initialization
+	void Start () {
+		handButtonImg = GameObject.Find(gameObject.name+"/handButton").GetComponent<RawImage> ();
+		handButton = GameObject.Find(gameObject.name+"/handButton").GetComponent<Selectable> ();
+		eventListener = GameObject.Find(gameObject.name+"/EventSystem").GetComponent<EventSystem> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*
+		if (handButton.OnSelect(eventListener))
+			Debug.Log ("handButton Selected!");
+		*/
+	}
+}
